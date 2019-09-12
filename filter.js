237301f6aa6f8e66e86935315c948057a33e30a6
Carlos Nobrega <nobregacarlosjr@gmail.com>
2019-09-12 20:36:46 -0300

    filter implementation

---
+++ filter.js
@@ -0,0 +1,17 @@
+Array.prototype.filter = function(predicate) {
+  const result = [];
+
+  for (let i = 0; i < this.length; i++) {
+    if (predicate(this[i])) result.push(this[i]);
+  }
+
+  return result;
+};
+
+Array.prototype.filterClean = function(predicate) {
+  const result = [];
+
+  for (const item of this) predicate(item) && result.push(item);
+
+  return result;
+};
